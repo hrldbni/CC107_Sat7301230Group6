bf728c82691466290cd43507c2af240342f5e465
Marvin Bernal <77772098+Vinbernal@users.noreply.github.com>
2021-03-09 14:48:11 +0800

    Drawing No More

---
+++ Drawing_No_More/app/src/main/java/com/example/myapplication/LoginPanel.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_panel);
+
+        TextView signUpNow = (TextView) findViewById(R.id.signUpNow);
+        signUpNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent;
+                newIntent = new Intent(LoginPanel.this, SignUpPanel.class);
+                startActivity(newIntent);
+                finish();
+            }
+        });
+
+    }
+}
