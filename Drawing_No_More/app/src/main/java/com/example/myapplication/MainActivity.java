d3bbfc3b9ab4c7c3c73d4a2ae577f1ab562457a0
JayCabasag <jaycabasag1999@gmail.com>
2021-04-07 23:18:07 +0800

    Fixed SplashSplash

--- Drawing_No_More/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Drawing_No_More/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,25 +10,35 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 //SPLASH SCREEN
     ImageView drawingText;
     com.airbnb.lottie.LottieAnimationView img;
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 5000;
+
+    ImageView logo,appname,splashImg;
+    LottieAnimationView lottieAnimationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
         getSupportActionBar().hide();
 
-        img = (com.airbnb.lottie.LottieAnimationView) findViewById(R.id.travelLogo);
-        drawingText = (ImageView) findViewById(R.id.drawingtext);
+        logo = findViewById(R.id.logo);
+        appname = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
 
-        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade);
-        img.startAnimation(animation);
-        drawingText.startAnimation(animation);
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appname.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
--- Drawing_No_More/app/src/main/java/com/example/myapplication/splashscreen.java
+++ Drawing_No_More/app/src/main/java/com/example/myapplication/splashscreen.java
@@ -19,21 +19,7 @@ public class splashscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        logo = findViewById(R.id.logo);
-        appname = findViewById(R.id.app_name);
-        splashImg = findViewById(R.id.img);
-        lottieAnimationView = findViewById(R.id.lottie);
 
-        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
-        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-        appname.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-
-                Intent homeIntent;
-                homeIntent = new Intent(splashscreen.this, LoginPanel.class);
-
-                startActivity(homeIntent);
-                finish();
 
 
     }
